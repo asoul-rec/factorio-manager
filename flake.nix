70858465a3862492ede015a7009000df4c94dbc6
oluceps <i@oluceps.uk>
2024-01-06 19:33:46 +0800

    add nix flake with direnv poetry env

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "flake for factorio manager";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    devenv.url = "github:cachix/devenv";
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = { self, nixpkgs, devenv, ... } @ inputs:
+    let
+      pkgs = nixpkgs.legacyPackages."x86_64-linux";
+    in
+    {
+      devShell.x86_64-linux = devenv.lib.mkShell {
+        inherit inputs pkgs;
+        modules = [
+          ({ pkgs, config, ... }: {
+            languages.python = {
+              enable = true;
+              poetry.enable = true;
+            };
+            dotenv.enable = true;
+            enterShell = ''
+              export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib"
+            '';
+          })
+        ];
+      };
+    };
+}
