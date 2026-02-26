474c62a292b8cf4da448188eb204f28f7aed8272
asoul-rec <asoulranbi@gmail.com>
2026-02-26 01:31:50 -0600

    Update grpc and proto file
    - change game_version to optional
    - update grpc and pyrofork dependency version
    - force dependency version to ensure environment match

--- facmgr/protobuf/facmgr_pb2_grpc.py
+++ facmgr/protobuf/facmgr_pb2_grpc.py
@@ -6,7 +6,7 @@ import warnings
 from facmgr.protobuf import facmgr_pb2 as facmgr_dot_protobuf_dot_facmgr__pb2
 from google.protobuf import empty_pb2 as google_dot_protobuf_dot_empty__pb2
 
-GRPC_GENERATED_VERSION = '1.68.0'
+GRPC_GENERATED_VERSION = '1.78.1'
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
@@ -19,7 +19,7 @@ except ImportError:
 if _version_not_supported:
     raise RuntimeError(
         f'The grpc package installed is at version {GRPC_VERSION},'
-        + f' but the generated code in facmgr/protobuf/facmgr_pb2_grpc.py depends on'
+        + ' but the generated code in facmgr/protobuf/facmgr_pb2_grpc.py depends on'
         + f' grpcio>={GRPC_GENERATED_VERSION}.'
         + f' Please upgrade your grpc module to grpcio>={GRPC_GENERATED_VERSION}'
         + f' or downgrade your generated code using grpcio-tools<={GRPC_VERSION}.'
